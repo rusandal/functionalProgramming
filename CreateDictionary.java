374fcc2f0de37bd56abf5d387d91b1b690a7e60f
sandal <ru-sandal@yandex.ru>
2021-12-06 11:09:49 +0300

    first commit

---
+++ CreateDictionary.java
@@ -0,0 +1,6 @@
+import java.util.List;
+
+@FunctionalInterface
+public interface CreateDictionary {
+    List<String> createDictionary(String text);
+}
---
+++ Main.java
@@ -0,0 +1,18 @@
+import java.util.List;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class Main{
+    final static String text = "Fsdfsdfsvv, vvvbnnnf sdd asddd";
+
+    public static void main(String[] args) {
+        Function<String, List<String>> dictionary = text -> Stream.of(text.split(" "))
+                .map(elem -> new String(elem).replaceAll("[^a-zA-Z ]", ""))
+                .distinct()
+                .map(s -> Character.toUpperCase(s.charAt(0)) + s.substring(1))
+                .sorted()
+                .collect(Collectors.toList());
+        System.out.println(dictionary.apply(text));
+    }
+}
